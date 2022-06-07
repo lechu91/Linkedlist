0a6859c6150265f7709971c1c40f53e45b9d5fdf
lechu91 <51946227+lechu91@users.noreply.github.com>
2022-06-06 19:54:40 -0700

    Add files via upload

---
+++ linked_list.py
@@ -0,0 +1,41 @@
+import hashlib
+from datetime import datetime
+
+
+class Block:
+    def __init__(self, index, timestamp, content, previous_hash):
+        self.index = index
+        self.timestamp = timestamp
+        self.content = content
+        self.previous_hash = previous_hash
+        self.hash = self.calc_hash()
+
+    def calc_hash(self):
+        sha = hashlib.sha256()
+        sha.update(str(self.index).encode('utf-8') +
+                   str(self.timestamp).encode('utf-8') +
+                   str(self.content).encode('utf-8') +
+                   str(self.previous_hash).encode('utf-8'))
+        return sha.hexdigest()
+
+
+M4BlockChain = []
+
+
+def create_genesis_block():
+    return Block(0, datetime.now(), "Genesis Block", "0")
+
+
+M4BlockChain.append(create_genesis_block())
+
+
+# write a function `next_block` to generate a block
+def next_block(last_block):
+    next_index = last_block.index + 1
+    return Block(next_index, datetime.now(), "this is block " + str(next_index), last_block.hash)
+
+
+# append 5 blocks to the blockchain
+def app_five(block_list):
+    for i in range(5):
+        M4BlockChain.append(next_block(M4BlockChain[-1]))
